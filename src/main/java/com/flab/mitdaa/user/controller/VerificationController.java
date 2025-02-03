f5f1010fdd377627dbd16b16f5dabab71f3f8535
hbson1 <hbson1@myseneca.ca>
2025-02-04 00:40:27 +0900

    리팩토링2

--- src/main/java/com/flab/mitdaa/user/controller/VerificationController.java
+++ src/main/java/com/flab/mitdaa/user/controller/VerificationController.java
@@ -2,6 +2,7 @@ package com.flab.mitdaa.user.controller;
 
 
 
+import com.flab.mitdaa.user.entity.User;
 import com.flab.mitdaa.user.entity.VerificationToken;
 import com.flab.mitdaa.user.service.VerificationService;
 import lombok.RequiredArgsConstructor;
@@ -20,17 +21,14 @@ public class VerificationController {
     private final VerificationService verificationService;
 
     @GetMapping("/verify")
-    public  ResponseEntity<String> verifyEmail(@RequestParam String token) { // void로 반환
+    public  void verifyEmail(@RequestParam String token) {
         log.info("token!!!!!!!!!" + token);
         verificationService.verifyEmail(token);
-        return ResponseEntity.ok("인증이 완료되었습니다.");
     }
 
 
     @PostMapping("/resend")
-    public ResponseEntity<String> resendEmail(@RequestBody Map<String, String> req ) {  // key 값을 알수가 없다, 스웨거에서도 확인 불가 . map 사용시 , 객체로 만들어서 수정 필요. void로 변경
-
-        verificationService.resendVerifyEmail(req.get("email"));
-        return ResponseEntity.ok("인증 이메일이 재전송 되었습니다.");
+    public void resendEmail(@RequestBody User req) {  // key 값을 알수가 없다, 스웨거에서도 확인 불가 . map 사용시 , 객체로 만들어서 수정 필요. void로 변경
+        verificationService.resendVerifyEmail(req.getEmail());
     }
 }
